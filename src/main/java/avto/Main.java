8d02f3e2a5ea557473ce03dfc23425b30449c745
Dmitrii Sobin <neo7976@yandex.ru>
2022-10-02 20:21:53 +0300

    Реализация покупки и ожидания

--- src/main/java/avto/Main.java
+++ src/main/java/avto/Main.java
@@ -8,13 +8,16 @@ public class Main {
 
     public final static int ISSUE = 10;
     public final static int PEOPLE = 4;
+    public final static int TIME_ADMISSION = 4000;
+    public final static int TIME_PEOPLE_BUY = 500;
 
     public static void main(String[] args) {
         List<CarImp> auto = new ArrayList<>();
+        Random random = new Random();
 
-        new Thread(() -> {
+        Thread carThread = new Thread(() -> {
             CarImp newCar = null;
-            Random random = new Random();
+
             int value;
             for (int i = 0; i < ISSUE; i++) {
                 value = random.nextInt(2);
@@ -31,32 +34,39 @@ public class Main {
                         }
                     }
                     newCar.admission();
-                    auto.notify();
                 }
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(TIME_ADMISSION);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-        }).start();
+        });
+        carThread.start();
 
 
         new Thread(() -> {
-            for (int i = 1; i < PEOPLE; i++) {
-                synchronized (auto) {
-                    if (auto.isEmpty()) {
-                        try {
-                            auto.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+            int i;
+            while (true) {
+                i = (int) (Math.random() * PEOPLE + 1);
+                System.out.println("Покупатель " + i + " зашёл в магазин");
+                if (!auto.isEmpty()) {
+                    synchronized (auto) {
+                        System.out.printf("Покупатель %d купил %s.\n",
+                                i,
+                                auto.remove(0));
                     }
-                    System.out.printf("Покупатель %d купил %s.\n", i, auto.remove(0));
+                } else
+                    System.out.println("Машин нет");
+
+                try {
+                    Thread.sleep(TIME_PEOPLE_BUY);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+                if (!carThread.isAlive())
+                    break;
             }
         }).start();
     }
-
-
 }
